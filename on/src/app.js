d4c1c829354524b20f8bff2938e1df6e3bc6e4cd
Jack Margeson <margeson.jack@gmail.com>
2019-07-27 16:00:31 -0400

    React.js testing

---
+++ on/src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
